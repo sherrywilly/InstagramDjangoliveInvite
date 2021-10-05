335e5c29ba761607d6cc66d161be692f48e7c15a
sherry wilson <sherry@deepsense.in>
2021-10-05 14:38:26 +0530

    commit

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,5 @@ from core.views import LoginView, dashboard,tester
 urlpatterns = [
 path('', LoginView, name="login"),
 path('dash/', dashboard, name="dash"),
-path('test/',tester,name="test")
+path('test/<user>/',tester,name="test")
 ]
