e54863645c63494a95d69776ce1c2ee22fa64fc7
sherry wilson <sherry@deepsense.in>
2021-10-10 14:13:31 +0530

    added treading

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from core.views import LoginView, UserManage, addSlave, dashboard, deleteSlave, logout_view, status_view,tester
+from core.views import LoginView, UserManage, addSlave, dashboard, deleteSlave, insta_invite, logout_view, status_view,tester,fetch_users
 
 urlpatterns = [
 path('', LoginView, name="login"),
@@ -10,5 +10,7 @@ path('test/<user>/',tester,name="test"),
     path('logout/', logout_view, name="logout"),
     path('addslave/', addSlave, name="addslave"),
     path('slave/<int:pk>/', deleteSlave, name="sremove"),
-    path('user/action/<uuid:pk>', UserManage, name='umanage')
+    path('user/action/<uuid:pk>', UserManage, name='umanage'),
+    path('fetch_users/',fetch_users,name="test"),
+    path('invite/',insta_invite,name="invitor"),
 ]
