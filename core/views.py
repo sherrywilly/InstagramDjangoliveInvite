63c351b475f77d485f7f248162cfed199c86eff1
sherry wilson <sherry@deepsense.in>
2021-10-10 16:49:18 +0530

    threading removed

--- core/views.py
+++ core/views.py
@@ -295,11 +295,24 @@ def insta_invite(request):
         return JsonResponse({"status":"ok","message":"No users available"})
     key = request.GET.get('key')
     if key == "mom":
-        for i in users:
-            # print(i.pk)
-            # x = i.pk
-            InstaInviteThread(i.pk).start()
-            # time.sleep(5)
+        for y in users:
+            try:
+                random_bit = random.getrandbits(1)
+                random_boolean = bool(random_bit)
+                if random_boolean:
+                    print("------------- SHORTCODE FROM REELS -------------")
+                    x = get_shortcode_from_reels_2(user=y)
+                else:
+                    print("------------- SHORTCODE FROM EXPLORE -------------")
+                    x = get_shortcode_from_explore(cookie=y.cookie)
+                x = x['items'][0]['media']['code']
+                    
+                x= get_users_from_shortcode(cookie=y.get_slave,shortcode=x)
+                y = IgUser.objects.get(id=y.id)
+                y.desc = y.desc+",".join(x)+","
+                y.save()
+            except Exception as x:
+                Status.objects.create(status='Fail',ig_id=y,comment=x,response="FAILED TO FETCH USERS")
     else:
         return JsonResponse({"status":"Fail"})
 
