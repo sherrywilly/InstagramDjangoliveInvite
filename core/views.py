c817c480ab2d3460f429fd0ac89cd2c62f6be014
sherry wilson <sherry@deepsense.in>
2021-10-10 17:07:12 +0530

    commit

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 import asyncio
+from django.db import transaction
 from django.http import response
 from django.http.response import JsonResponse
 # from core.decorators import session_not_exist
@@ -297,22 +298,14 @@ def insta_invite(request):
     if key == "mom":
         for y in users:
             try:
-                random_bit = random.getrandbits(1)
-                random_boolean = bool(random_bit)
-                if random_boolean:
-                    print("------------- SHORTCODE FROM REELS -------------")
-                    x = get_shortcode_from_reels_2(user=y)
-                else:
-                    print("------------- SHORTCODE FROM EXPLORE -------------")
-                    x = get_shortcode_from_explore(cookie=y.cookie)
-                x = x['items'][0]['media']['code']
-                    
-                x= get_users_from_shortcode(cookie=y.get_slave,shortcode=x)
-                y = IgUser.objects.get(id=y.id)
-                y.desc = y.desc+",".join(x)+","
-                y.save()
-            except Exception as x:
-                Status.objects.create(status='Fail',ig_id=y,comment=x,response="FAILED TO FETCH USERS")
+                with transaction.atomic():
+                    x = y.desc
+                #   json.loads(y.json)
+                    IgUser.objects.filter(username=y.username,active=True).update(desc="4619342150,")
+                    j =get_user_by_id(user=y,user_id=x)
+                    Status.objects.create(ig_id=y,comment=x,response=j)
+            except Exception as e:
+                Status.objects.create(ig_id=y,comment="SOME THING WENT WRONG IN INVITE",response=e,status="Fail")
     else:
         return JsonResponse({"status":"Fail"})
 
