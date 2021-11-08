50e1d20b1f12818f14781aa86ad7ea358a4c3fd1
sherry wilson <sherry@deepsense.in>
2021-11-08 07:34:23 +0530

    upload datasize changed

--- instaDjangoApi/settings.py
+++ instaDjangoApi/settings.py
@@ -140,7 +140,7 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-
+DATA_UPLOAD_MAX_MEMORY_SIZE = 5242880
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
