a328de4386af2548b7639cb48bd58c1f2f536d3d
sherrywilly <sherrywilson521@gmail.com>
2022-05-22 04:02:45 -0700

    node js dockerized with django

--- app/router/demo.js
+++ app/router/demo.js
@@ -16,52 +16,53 @@ var HttpsProxyAgent = require('https-proxy-agent');
 router.get('/', (req, res) => {
 
     // let cookie = req.params.Cookie;
-    var cookie = { "mid": "YokcTAABAAFQ9GHLUgjHhPgcOoSG", "rur": "NCG", "csrftoken": "HY30xuKGOZEQ3KjJZBtQNEcMSs8YrTiB", "sessionid": "9657000400%3AhmSH5hJhV0UO5g%3A1", "ds_user_id": "9657000400" }
-    let format_cookie = ''
-    for (let key in cookie) {
-        format_cookie += key + '=' + cookie[key] + ';'
-    }
-    let headers = {
-        'Content-Type': 'application/Json',
-        'user-agent': ua,
-        'Cookie': format_cookie,
-    }
-    let proxy = 'http://sKMty7:JUsYZ2@45.145.57.224:10524'
+    // var cookie = { "mid": "YokcTAABAAFQ9GHLUgjHhPgcOoSG", "rur": "NCG", "csrftoken": "HY30xuKGOZEQ3KjJZBtQNEcMSs8YrTiB", "sessionid": "9657000400%3AhmSH5hJhV0UO5g%3A1", "ds_user_id": "9657000400" }
+    // let format_cookie = ''
+    // for (let key in cookie) {
+    //     format_cookie += key + '=' + cookie[key] + ';'
+    // }
+    // // let headers = {
+    // //     'Content-Type': 'application/Json',
+    // //     'user-agent': ua,
+    // //     'Cookie': format_cookie,
+    // // }
+    // let proxy = 'http://sKMty7:JUsYZ2@45.145.57.224:10524'
 
-    var options = {
-        'method': 'POST',
-        'url': 'https://i.instagram.com/api/v1/story_interactions/send_story_like/',
-        'proxy': proxy,
-        'headers': {
-            'Cookie': format_cookie,
-            'User-Agent': 'Instagram 231.0.0.18.113 Android (11\\/3.3.1; 120; 480x800; samsung; GT-N7000; GT-N7000; smdkc210; en_US)',
-            'Content-Type': 'application/json',
-        },
+    // var options = {
+    //     'method': 'POST',
+    //     'url': 'https://i.instagram.com/api/v1/story_interactions/send_story_like/',
+    //     'proxy': proxy,
+    //     'headers': {
+    //         'Cookie': format_cookie,
+    //         'User-Agent': 'Instagram 231.0.0.18.113 Android (11\\/3.3.1; 120; 480x800; samsung; GT-N7000; GT-N7000; smdkc210; en_US)',
+    //         'Content-Type': 'application/json',
+    //     },
 
-        body: JSON.stringify({
-            "media_id": "2446597535621417675",
-            "container_module": "reel_feed_timeline"
-        })
+    //     body: JSON.stringify({
+    //         "media_id": "2446597535621417675",
+    //         "container_module": "reel_feed_timeline"
+    //     })
 
-    };
-    let axiosConfig = {
-        headers: {
-            'Cookie': format_cookie,
-            'User-Agent': 'Instagram 231.0.0.18.113 Android (11\\/3.3.1; 120; 480x800; samsung; GT-N7000; GT-N7000; smdkc210; en_US)',
-            'Content-Type': 'application/json'
-        }
-    }
-    let postDate = {
-        "media_id": "2446597535621417675",
-        "container_module": "reel_feed_timeline"
-    }
-    axios.post(url, postDate, axiosConfig)
-        .then(function (response) {
-            console.log(response.data);
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+    // };
+    // let axiosConfig = {
+    //     headers: {
+    //         'Cookie': format_cookie,
+    //         'User-Agent': 'Instagram 231.0.0.18.113 Android (11\\/3.3.1; 120; 480x800; samsung; GT-N7000; GT-N7000; smdkc210; en_US)',
+    //         'Content-Type': 'application/json'
+    //     }
+    // }
+    // let postDate = {
+    //     "media_id": "2446597535621417675",
+    //     "container_module": "reel_feed_timeline"
+    // }
+    // let url = 'https://i.instagram.com/api/v1/story_interactions/send_story_like/'
+    // axios.post(url, postDate, axiosConfig)
+    //     .then(function (response) {
+    //         console.log(response.data);
+    //     })
+    //     .catch(function (error) {
+    //         console.log(error);
+    //     });
 
     // axios with proxy
     // request(options, function (error, response, body) {
@@ -105,11 +106,11 @@ router.post('/', (req, res) => {
         format_cookie += key + '=' + cookie[key] + ';'
     }
     let ua = 'Instagram 231.0.0.18.113 Android (11\\/3.3.1; 120; 480x800; samsung; GT-N7000; GT-N7000; smdkc210; en_US)'
-    let headers = {
-        'Content-Type': 'application/Json',
-        'user-agent': ua,
-        'Cookie': format_cookie,
-    }
+    // let headers = {
+    //     'Content-Type': 'application/Json',
+    //     'user-agent': ua,
+    //     'Cookie': format_cookie,
+    // }
     let axiosConfig = {
         headers: {
             'Cookie': format_cookie,
